6244e0c1bc73163c3c833ed111c37ce1951c1c16
idleberg <git@idleberg.com>
2020-08-19 10:30:18 +0200

    merge files

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,3 +2,26 @@ declare module '*.json' {
   const value: any;
   export default value;
 }
+
+interface FlagOptions {
+  algorithm?: string;
+  failFast?: boolean;
+  print?: boolean;
+  output?: string;
+  sort?: string;
+  winsfv?: string;
+}
+
+interface DateObject {
+  year: string;
+  month: string;
+  day: string;
+  hours: string;
+  minutes: string;
+  seconds: string;
+}
+
+interface SFVObject {
+  checksum: string;
+  file: string;
+}
