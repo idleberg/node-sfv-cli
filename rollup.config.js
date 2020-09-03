ab0251ae01295689dbffb1359b470748f10d287d
idleberg <git@idleberg.com>
2020-09-04 00:34:33 +0200

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,26 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
 
-const plugins = [
-  commonjs(),
-  json(),
-  typescript({
-    allowSyntheticDefaultImports: true
-  })
-];
+const defaults = {
+  external: [ 'crypto' ],
+  output: {
+    dir: 'bin',
+    format: 'cjs'
+  },
+  plugins: [
+    commonjs(),
+    json(),
+    nodeResolve(),
+    typescript({
+      allowSyntheticDefaultImports: true,
+      moduleResolution: 'node'
+    })
+  ]
+};
 
-export default [
-  {
-    input: './src/cli.ts',
-    output: {
-      dir: 'bin',
-      format: 'cjs'
-    },
-    plugins: plugins
-  }
-];
+export default {
+  ...defaults,
+  input: 'src/cli.ts'
+};
