14c2c1877ae76270f093976b7042991b4d6776b2
idleberg <git@idleberg.com>
2020-09-05 20:36:53 +0200

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,24 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
 import json from '@rollup/plugin-json';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 import typescript from '@rollup/plugin-typescript';
 
 const defaults = {
-  external: [ 'crypto' ],
+  external: [
+    'crypto',
+    'fs'
+  ],
   output: {
     dir: 'bin',
     format: 'cjs'
   },
   plugins: [
     commonjs(),
+    filesize(),
     json(),
+    nodePolyfills(),
     nodeResolve(),
     typescript({
       allowSyntheticDefaultImports: true,
