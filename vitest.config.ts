a9f3aaaa4d78f7ce61fe2bc4e9ea6c99cf33a1c6
Jan T. Sott <git@idleberg.com>
2025-12-31 14:12:27 +0100

    chore: add vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		include: ['e2e/*.spec.ts', 'src/*.spec.ts'],
+		coverage: {
+			exclude: ['src/index.*.ts', 'e2e/*.ts'],
+			include: ['src/*.ts'],
+		},
+		// Hide all CLI output in tests
+		onConsoleLog: () => false,
+	},
+});
