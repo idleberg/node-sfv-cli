7b08eabb38eab20d194ef9e88329552fcc5ec9ca
Jan T. Sott <git@idleberg.com>
2026-01-06 23:19:50 +0100

    chore: update exclude pattern

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 	test: {
 		include: ['e2e/*.spec.ts', 'src/*.spec.ts'],
 		coverage: {
-			exclude: ['src/index.*.ts', 'e2e/*.ts'],
+			exclude: ['src/index.ts', 'e2e/*'],
 			include: ['src/*.ts'],
 		},
 		// Hide all CLI output in tests
